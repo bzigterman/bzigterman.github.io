f901cd87a93901c7eb9b7b58fbbf85037820d6f8
Ben Zigterman <ben@bzigterman.com>
2021-10-12 11:31:57 -0500

    add bbc to world news

--- news.R
+++ news.R
@@ -10,12 +10,16 @@ nyt <- tidyfeed("https://rss.nytimes.com/services/xml/rss/nyt/World.xml") %>%
 wsj <- tidyfeed("https://feeds.a.dj.com/rss/RSSWorldNews.xml")%>%
   select(feed_title, item_pub_date,item_title, item_link, item_description)%>%
   mutate(feed = "WSJ")
-# politico <- tidyfeed("http://rss.politico.com/politics.xml")%>%
-  #select(feed_title, item_pub_date,item_title, item_link, item_description)%>%
-  #mutate(feed = "Politico")
+bbc <- tidyfeed("http://feeds.bbci.co.uk/news/world/rss.xml")%>%
+  select(feed_title, item_pub_date,item_title, item_link, item_description)%>%
+  mutate(feed = "BBC")
 
-news <- full_join(nyt, wsj) %>%
-  #full_join(politico) %>%
+# politico <- tidyfeed("http://rss.politico.com/politics.xml")%>%
+#select(feed_title, item_pub_date,item_title, item_link, item_description)%>%
+#mutate(feed = "Politico")
+
+world_news <- full_join(nyt, wsj) %>%
+  full_join(bbc) %>%
   arrange(desc(item_pub_date)) %>%
   mutate(item_md_link = paste("[",item_title,"](",item_link,")",
                               sep = "")) %>%
@@ -23,34 +27,9 @@ news <- full_join(nyt, wsj) %>%
                                        sep = "")) 
 
 
-# check if new ----
-# # old_news <- as_tibble(2)
-# old_news <- read_csv("data/news.csv",
-#                            col_types = cols())
-#                              
-#                           #   league = col_character(),
-#                           #   team_label = col_character(),
-#                           #   wins = col_number(),
-#                           #   losses = col_number(),
-#                           #   win_pct_text = col_character(),
-#                           #   games_remaining = col_number(),
-#                           #   last_ten = col_character()),
-#                           # trim_ws = FALSE
-#                           
-# news_checker <- news %>%
-#   select(item_title, feed)
-#                              
-#                              
-# news_the_same <- all_equal(news_checker, old_news)
-# 
-# if (news_the_same != TRUE) { 
-#   write_csv(news_checker,"data/news.csv")
-# }
-
-
 # create table ----
 
-news_table <- news %>%
+world_news_table <- world_news %>%
   select(item_md_link, feed_plus_description) %>%
   gt() %>%
   fmt_markdown(item_md_link) %>%
@@ -59,10 +38,10 @@ news_table <- news %>%
     feed_plus_description = ""
   ) %>% 
   opt_table_lines(extent = c("none"))
-news_table
+world_news_table
 
 
-news_table_html <- as_raw_html(news_table)
+world_news_table_html <- as_raw_html(world_news_table)
 
 
 # web text ----
@@ -76,7 +55,7 @@ permalink: /charts/news/
 
 ## World News
 
-",news_table_html,"
+",world_news_table_html,"
 
 ",
 sep = ""
