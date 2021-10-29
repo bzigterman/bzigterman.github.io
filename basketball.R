592254a86ca8fb2e10d1d87d92b97e34644a05f9
Ben Zigterman <ben@bzigterman.com>
2021-10-28 21:24:18 -0500

    adjust web text

--- basketball.R
+++ basketball.R
@@ -325,8 +325,6 @@ permalink: /projects/basketball/
 
 ",now_html," 
 
-## Standings
-
 ![Standings]({{ site.baseurl }}/plots/nba_team_rank.png)
 
 <p class=\"updated_time\">Source: <a href=\"https://github.com/fivethirtyeight/data/tree/master/nba-forecasts\">FiveThirtyEight</a>. <a href=\"https://github.com/fivethirtyeight/data/blob/master/LICENSE\">CC-BY-4.0 License</a>.</p> 
