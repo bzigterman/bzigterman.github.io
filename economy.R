c7bb2dc85187ca6aacbf0c9e1545df66041a82fc
Ben Zigterman <ben@bzigterman.com>
2021-11-14 13:44:39 -0600

    convert IL md to html

--- economy.R
+++ economy.R
@@ -848,9 +848,9 @@ permalink: /projects/economy/
 <details>
 <summary><h2>Illinois</h2></summary>
 
-![Flash Index]({{ site.baseurl }}/plots/il_flash_index.png)
+<img src="{{ site.baseurl }}/plots/il_flash_index.png" alt="Flash Index">
 
-Source: [Institute of Government and Public Affairs at the University of Illinois](https://igpa.uillinois.edu/page/flash-index-archive).
+Source: <a href="https://igpa.uillinois.edu/page/flash-index-archive">Institute of Government and Public Affairs at the University of Illinois</a>
 
 </details>
 
