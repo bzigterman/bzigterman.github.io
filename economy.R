f1db43ef7857b3daada4a0651c451fdcbb364e4f
Ben Zigterman <ben@bzigterman.com>
2022-02-07 10:45:28 -0600

    remove duplicate link

--- economy.R
+++ economy.R
@@ -951,8 +951,6 @@ imageurl: https://bzigterman.com/plots/champaign_unemployment_rate.png
 
 [![Flash Index]({{ site.baseurl }}/plots/il_flash_index.png)](https://igpa.uillinois.edu/page/flash-index-archive)
 
-Source: [Institute of Government and Public Affairs at the University of Illinois](https://igpa.uillinois.edu/page/flash-index-archive).
-
 [![Population]({{ site.baseurl }}/plots/il_population.png)](https://fred.stlouisfed.org/series/ILPOP)
 
 ## United States
