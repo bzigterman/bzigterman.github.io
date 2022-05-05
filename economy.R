68aeac94ac61c13c518db7d42a825f1e65c8640f
Ben Zigterman <ben@bzigterman.com>
2022-05-05 09:10:05 -0500

    adjust height of champaign housing

--- economy.R
+++ economy.R
@@ -1490,7 +1490,7 @@ imageurl: https://bzigterman.com/plots/champaign_unemployment_rate.png
 <iframe src=\"/interactive/champaign_employment.html\" width=\"100%\" height=\"500\"> 
 </iframe>
 
-<iframe src=\"/interactive/champaign_housing.html\" width=\"100%\" height=\"700\"> 
+<iframe src=\"/interactive/champaign_housing.html\" width=\"100%\" height=\"600\"> 
 </iframe>
 
 ",better_cu_housing_table_html,"
