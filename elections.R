8318c7d6002ee1e740ac542a1a19a002f83df9f9
Ben Zigterman <ben@bzigterman.com>
2022-06-30 22:16:44 -0500

    adjust election code

--- elections.R
+++ elections.R
@@ -4,9 +4,6 @@ library(readr)
 
 results_url <- "https://ccco-results.s3.us-east-2.amazonaws.com/2022/docs/june/EL45A.HTM"
 results_html <- download.file(url = results_url, destfile = "results.txt")
-read.table(file = "results.txt")
-results <- read_lines(file = "results.txt", skip = 8) 
-results
 fwf <- read_fwf(file = "results.txt",
          fwf_cols(name = 40, total = 12, pct = 8, early = 14, mail = 14, election_day = 14),
          col_types = "cnnnnn",
@@ -17,5 +14,7 @@ fwf_clean <- fwf %>%
   filter(name != "</PRE>") %>%
   filter(!grepl("Over Votes", name)) %>%
   filter(!grepl("Under Votes", name)) %>%
-  filter(!grepl("VOTE FOR", name))
-         
+  filter(!grepl("VOTE FOR", name)) %>%
+  select(name, total)
+
+
