f2eb451f95b82a7a1b6f3127ad20849239e48504
Ben Zigterman <ben@bzigterman.com>
2022-07-07 09:37:07 -0500

    change step position

--- weather.R
+++ weather.R
@@ -773,7 +773,7 @@ fig <- hchart(year_weather_data_longer, "arearange",
                     low = round(min),
                     high = round(max),
                     group = type),
-              step = "right",
+              step = "center",
               marker = list(
                 radius = 1),
               lineWidth = 0,
