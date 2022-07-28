c5941a822785dfb0f4faf9a35d6aac31393325fb
Ben Zigterman <ben@bzigterman.com>
2022-07-27 22:20:14 -0500

    add news back

--- news.R
+++ news.R
@@ -9,26 +9,69 @@ past_week <- ymd_hms(now()) - days(3)
 # gather news ----
 
 ## politics ----
-# memeorandum_html <- tidyfeed("https://www.memeorandum.com/feed.xml", clean_tags = FALSE) %>%
-#   mutate(url = str_extract(item_description, "<A HREF=\"(.*?)\""))
-# memeorandum_html$url
-# memeorandum_html$item_description <- memeorandum_html$item_description %>%
-#   read_html()
+memeorandum_html <- read_html("https://www.memeorandum.com/feed.xml", options = "NOCDATA")
 
-memeorandum <- tidyfeed("https://www.memeorandum.com/feed.xml") %>%
-  select(feed_title, item_pub_date,item_title, item_link, item_description) %>%
+links <- memeorandum_html %>%
+  html_elements("description") %>% 
+  html_elements("b") %>% 
+  html_elements("a") %>% 
+  html_attr("href")
+links
+titles <- memeorandum_html %>%
+  html_elements("description") %>% 
+  html_elements("b") %>% 
+  html_elements("a") %>% 
+  html_text()
+titles
+pubdate <- memeorandum_html %>%
+  html_elements("pubdate") %>%
+  html_text() %>%
+  tail(-1)
+pubdate
+pubdate <- memeorandum_html %>%
+  html_elements("pubdate") %>%
+  html_text() %>%
+  tail(-1)
+pubdate
+publication <- memeorandum_html %>%
+  html_elements("p") %>%
+  html_elements("a") %>%
+  html_text() 
+publication <- publication[ c( FALSE, TRUE, FALSE ) ]
+publication
+description <- memeorandum_html %>%
+  html_elements("description") %>%
+  html_text() %>%
+  tail(-1)
+description
+memeorandum <- data.frame(
+  item_title = titles,
+  item_description = description,
+  item_pub_date = dmy_hms( pubdate, tz = "America/New_York"),
+  item_link = links,
+  feed = publication
+)
+
+politics_data <- read_csv(file = "data/politics_news.csv") 
+politics_news_update <- full_join(memeorandum,politics_data) %>%
+  unique() %>%
+  arrange(desc(item_pub_date)) %>%
+  filter(item_pub_date > past_week)
+
+write_csv(x = politics_news_update,
+          file = "data/politics_news.csv")
+
+
+memeorandum_feed <- politics_news_update %>%
   mutate(utc_time = force_tz(item_pub_date, tz = "US/Eastern")) %>%
   mutate(central_time = with_tz(utc_time, tz = "America/Chicago")) %>%
   mutate(clean_time = strftime(x = central_time, 
-                               tz = "US/Central",
+                               tz = "US/Eastern",
                                format = "%I:%M% %p CT, %b. %d")) %>%
-  mutate(outlet1 = str_extract(item_description, "(.*?):")) %>%
-  mutate(outlet2 = str_replace(outlet1, ".*(?=/)","")) %>%
-  mutate(feed = str_trim(str_extract(outlet2, "[^/].[^:]*"))) %>%
-  #select(feed_title, item_pub_date,item_title, item_link, item_description, feed) %>%
-  filter(feed == "CNN" | feed == "ABC News" |feed == "Politico" |feed == "Associated Press" |feed == "NBC News" |feed == "New York Times" |feed == "Atlanta Journal-Constitution" |feed == "Los Angeles Times" |feed == "The Guardian"|feed == "Reuters"|feed == "CBS News"|feed == "CNBC"|feed == "CBS News"|feed == "BBC")
+  filter(feed == "CNN" | feed == "ABC News" |feed == "Politico" |feed == "Washington Post" |feed == "Associated Press" |feed == "NBC News" |feed == "New York Times" |feed == "Atlanta Journal-Constitution" |feed == "Los Angeles Times" |feed == "The Guardian"|feed == "Reuters"|feed == "CBS News"|feed == "CNBC"|feed == "CBS News"|feed == "BBC"|feed == "Axios") %>%
+  filter(!grepl("opinion",item_link))
 
-politics_news <- memeorandum %>%
+politics_news <- memeorandum_feed %>%
   filter(!is.na(item_description)) %>%
   filter(!grepl("Opinion",item_title)) %>%
   filter(central_time > past_week) %>%
@@ -39,39 +82,10 @@ politics_news <- memeorandum %>%
   mutate(item_html_link = paste("<a href=\"",
                                 item_link,"\">",
                                 item_title,"</a>",
-                                sep = "")) %>%
-  mutate(feed_plus_description = paste(feed,": ",item_description,
-                                       sep = ""))
+                                sep = "")) 
 
-techmeme <- tidyfeed("https://www.techmeme.com/feed.xml") %>%
-  select(feed_title, item_pub_date,item_title, item_link, item_description) %>%
-  mutate(utc_time = force_tz(item_pub_date, tz = "US/Eastern")) %>%
-  mutate(central_time = with_tz(utc_time, tz = "America/Chicago")) %>%
-  mutate(clean_time = strftime(x = central_time, 
-                               tz = "US/Central",
-                               format = "%I:%M% %p CT, %b. %d")) %>%
-  mutate(outlet1 = str_extract(item_description, "(.*?):")) %>%
-  mutate(outlet2 = str_replace(outlet1, ".*(?=/)","")) %>%
-  mutate(feed = str_trim(str_extract(outlet2, "[^/].[^:]*"))) %>%
-  #select(feed_title, item_pub_date,item_title, item_link, item_description, feed) %>%
-  filter(feed == "Bloomberg" | feed == "The Verge" |feed == "Wall Street Journal" |feed == "9to5Mac" |feed == "MacRumors" |feed == "New York Times" |feed == "Ars Technica" |feed == "Rest of World" |feed == "The Guardian"|feed == "Reuters"|feed == "CBS News"|feed == "CNBC"|feed == "CBS News"|feed == "BBC")
 
-tech_news <- techmeme %>%
-  filter(!is.na(item_description)) %>%
-  filter(!grepl("Opinion",item_title)) %>%
-  filter(central_time > past_week) %>%
-  arrange(desc(central_time)) %>%
-  distinct(item_link, .keep_all = TRUE) %>%
-  mutate(item_md_link = paste("[",item_title,"](",item_link,")",
-                              sep = "")) %>%
-  mutate(item_html_link = paste("<a href=\"",
-                                item_link,"\">",
-                                item_title,"</a>",
-                                sep = "")) %>%
-  mutate(feed_plus_description = paste(feed,": ",item_description,
-                                       sep = ""))
 
-### create list ----
 politics_news_lines <- c()
 for (x in 1:nrow(politics_news)) {
   line=paste("<p class=\"updated_time\">",
@@ -85,15 +99,92 @@ for (x in 1:nrow(politics_news)) {
 }
 politics_news_lines 
 
-### create list ----
+
+## tech ----
+memeorandum_html <- read_html("https://www.techmeme.com/feed.xml", options = "NOCDATA")
+
+links <- memeorandum_html %>%
+  html_elements("description") %>% 
+  html_elements("b") %>% 
+  html_elements("a") %>% 
+  html_attr("href")
+links
+titles <- memeorandum_html %>%
+  html_elements("description") %>% 
+  html_elements("b") %>% 
+  html_elements("a") %>% 
+  html_text()
+titles
+pubdate <- memeorandum_html %>%
+  html_elements("pubdate") %>%
+  html_text() %>%
+  tail(-1)
+pubdate
+pubdate <- memeorandum_html %>%
+  html_elements("pubdate") %>%
+  html_text() %>%
+  tail(-1)
+pubdate
+publication <- memeorandum_html %>%
+  html_elements("description") %>%
+  html_elements("a") %>%
+  html_text() 
+publication <- publication[ c( FALSE, TRUE, FALSE ) ]
+publication
+description <- memeorandum_html %>%
+  html_elements("description") %>%
+  html_text() %>%
+  tail(-1)
+description
+memeorandum <- data.frame(
+  item_title = titles,
+  item_description = description,
+  item_pub_date = dmy_hms( pubdate, tz = "America/New_York"),
+  item_link = links,
+  feed = publication
+)
+
+politics_data <- read_csv(file = "data/tech_news.csv") 
+politics_news_update <- full_join(memeorandum,politics_data) %>%
+  unique() %>%
+  arrange(desc(item_pub_date)) %>%
+  filter(item_pub_date > past_week)
+
+write_csv(x = politics_news_update,
+          file = "data/tech_news.csv")
+
+
+memeorandum_feed <- politics_news_update %>%
+  mutate(utc_time = force_tz(item_pub_date, tz = "US/Eastern")) %>%
+  mutate(central_time = with_tz(utc_time, tz = "America/Chicago")) %>%
+  mutate(clean_time = strftime(x = central_time, 
+                               tz = "US/Central",
+                               format = "%I:%M% %p CT, %b. %d")) %>%
+  filter(feed == "Bloomberg" | feed == "The Verge" |feed == "Wall Street Journal" |feed == "9to5Mac" |feed == "MacRumors" |feed == "New York Times" |feed == "Ars Technica" |feed == "Rest of World" |feed == "The Guardian"|feed == "Reuters"|feed == "CBS News"|feed == "CNBC"|feed == "CBS News"|feed == "BBC") %>%
+  filter(!grepl("opinion",item_link))
+
+politics_news <- memeorandum_feed %>%
+  filter(!is.na(item_description)) %>%
+  filter(!grepl("Opinion",item_title)) %>%
+  filter(central_time > past_week) %>%
+  arrange(desc(central_time)) %>%
+  distinct(item_link, .keep_all = TRUE) %>%
+  mutate(item_md_link = paste("[",item_title,"](",item_link,")",
+                              sep = "")) %>%
+  mutate(item_html_link = paste("<a href=\"",
+                                item_link,"\">",
+                                item_title,"</a>",
+                                sep = "")) 
+
+
 tech_news_lines <- c()
-for (x in 1:nrow(tech_news)) {
+for (x in 1:nrow(politics_news)) {
   line=paste("<p class=\"updated_time\">",
-             tech_news$clean_time[[x]],
+             politics_news$clean_time[[x]],
              "</p>",
              "\n",
-             tech_news$item_html_link[[x]], 
-             tech_news$feed_plus_description[[x]],
+             politics_news$item_html_link[[x]], 
+             politics_news$item_description[[x]],
              "\n")
   tech_news_lines = paste(tech_news_lines, line)
 }
