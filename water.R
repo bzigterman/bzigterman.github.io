a2c98fe0114ddceafe3aa4465445c12ec88b9551
Ben Zigterman <ben@bzigterman.com>
2022-10-05 21:46:25 -0500

    adjust formatting

--- water.R
+++ water.R
@@ -39,6 +39,7 @@ mead_records <- full_join(page_1,
 
 latest_mead <- tail(mead_records,1)$value
 latest_mead_text <- format(round(tail(mead_records,1)$value), big.mark = ",")
+
 year_ago_mead <- head(tail(mead_records, 365),1)$value
 year_ago_mead_text <- 
   format(round(head(tail(mead_records, 365),1)$value), big.mark = ",")
@@ -54,12 +55,13 @@ year_change_mead_text <-
 year_change_mead_value <- value <- latest_mead - year_ago_mead
 year_change_mead_value_text <- 
   if (year_change_mead_value > 0) { 
-    paste("up ",year_change_mead_value,"", sep = "")
+    paste("up ",format(round(year_change_mead_value),big.mark = ","),"", sep = "")
   } else if (year_change_mead_value == 0) {
     paste("unchanged", sep = "")
   } else { 
-    paste("down ",year_change_mead_value,"", sep = "")
+    paste("down ",format(round(year_change_mead_value),big.mark = ","),"", sep = "")
   }
+
 ten_year_ago_mead <- head(tail(mead_records, 365*10),1)$value
 ten_year_ago_mead_text <- 
   format(round(head(tail(mead_records, 365*10),1)$value), big.mark = ",")
@@ -75,11 +77,11 @@ ten_year_change_mead_text <-
 ten_year_change_mead_value <- value <- latest_mead - ten_year_ago_mead
 ten_year_change_mead_value_text <- 
   if (ten_year_change_mead_value > 0) { 
-    paste("up ",ten_year_change_mead_value,"", sep = "")
+    paste("up ",format(round(ten_year_change_mead_value), big.mark = ","),"", sep = "")
   } else if (ten_year_change_mead_value == 0) {
     paste("unchanged", sep = "")
   } else { 
-    paste("down ",ten_year_change_mead_value,"", sep = "")
+    paste("down ",format(round(ten_year_change_mead_value), big.mark = ","),"", sep = "")
   }
 
 
@@ -109,15 +111,45 @@ latest_powell <- tail(powell_records,1)$value
 latest_powell_text <- format(round(tail(powell_records,1)$value), big.mark = ",")
 year_ago_powell <- head(tail(powell_records, 365),1)$value
 year_ago_powell_text <- 
-  format(round(head(tail(powell_records, 365),1)$value),big.mark = ",")
+  format(round(head(tail(powell_records, 365),1)$value), big.mark = ",")
 year_change_powell <- round((100*(latest_powell-year_ago_powell)/year_ago_powell),1)
 year_change_powell_text <- 
   if (year_change_powell > 0) { 
-    paste("+",year_change_powell,"%↑", sep = "")
+    paste("+",year_change_powell,"%", sep = "")
   } else if (year_change_powell == 0) {
-    paste("",year_change_powell,"%→", sep = "")
+    paste("",year_change_powell,"%", sep = "")
   } else { 
-    paste("",year_change_powell,"%↓", sep = "")
+    paste("",year_change_powell,"%", sep = "")
+  }
+year_change_powell_value <- value <- latest_powell - year_ago_powell
+year_change_powell_value_text <- 
+  if (year_change_powell_value > 0) { 
+    paste("up ",format(round(year_change_powell_value),big.mark = ","),"", sep = "")
+  } else if (year_change_powell_value == 0) {
+    paste("unchanged", sep = "")
+  } else { 
+    paste("down ",format(round(year_change_powell_value),big.mark = ","),"", sep = "")
+  }
+ten_year_ago_powell <- head(tail(powell_records, 365*10),1)$value
+ten_year_ago_powell_text <- 
+  format(round(head(tail(powell_records, 365*10),1)$value), big.mark = ",")
+ten_year_change_powell <- round((100*(latest_powell-ten_year_ago_powell)/ten_year_ago_powell),1)
+ten_year_change_powell_text <- 
+  if (ten_year_change_powell > 0) { 
+    paste("+",ten_year_change_powell,"%", sep = "")
+  } else if (ten_year_change_powell == 0) {
+    paste("",ten_year_change_powell,"%", sep = "")
+  } else { 
+    paste("",ten_year_change_powell,"%", sep = "")
+  }
+ten_year_change_powell_value <- value <- latest_powell - ten_year_ago_powell
+ten_year_change_powell_value_text <- 
+  if (ten_year_change_powell_value > 0) { 
+    paste("up ",format(round(ten_year_change_powell_value),big.mark = ","),"", sep = "")
+  } else if (ten_year_change_powell_value == 0) {
+    paste("unchanged", sep = "")
+  } else { 
+    paste("down ",format(round(ten_year_change_powell_value),big.mark = ","),"", sep = "")
   }
 
 ## charts ----
@@ -223,8 +255,8 @@ Elevation:
 
 Elevation:
 - Latest: ",latest_powell_text," feet
-- Year ago: ",year_ago_powell_text," feet
-- Change: ",year_change_powell_text,"
+- ",year_change_powell_value_text," feet, or ",year_change_powell_text," from a year ago
+- ",ten_year_change_powell_value_text," feet, or ",ten_year_change_powell_text," from ten years ago
 
 <p class=\"updated_time\">Source: <a href=\"https://www.usbr.gov/lc/region/g4000/riverops/hourly7.html#t0\">U.S. Bureau of Reclamation</a>.</p> 
 
