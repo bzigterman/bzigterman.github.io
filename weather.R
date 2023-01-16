c97a26cc4a36cd4b69a71f1fbdc9bed3d7e08c76
Ben Zigterman <ben@bzigterman.com>
2023-01-16 12:39:13 -0600

    add ceilings

--- weather.R
+++ weather.R
@@ -220,7 +220,15 @@ fig <- highchart() |>
                 yAxis = 6) |> 
   hc_yAxis_multiples(create_axis(naxis = 7, 
                                  heights = c(1,1,1,1,1,1,1),
-                                 title = list(text = NULL))) |> 
+                                 title = list(text = NULL),
+                                 ceiling = c(1000,
+                                             100,
+                                             1000,
+                                             100,
+                                             1000,
+                                             100,
+                                             1000
+                                 ))) |> 
   hc_xAxis(type = "datetime",
            gridLineColor = "#e9e8df",
            gridLineWidth = 1,
