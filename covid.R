2cc44ed42e219ca1f517fb94f2ccfeb421e4c988
Ben Zigterman <ben@bzigterman.com>
2023-02-21 18:25:07 -0600

    hopefully fix covid data issue

--- covid.R
+++ covid.R
@@ -310,14 +310,16 @@ saveWidget(widget = fig3, file = "interactive/il_transmission_levels.html",
 ### idph ----
 champaignpop <- 209983
 
-nyt_data <- full_join(rio::import("https://github.com/nytimes/covid-19-data/raw/master/us-counties-2020.csv",
-                                  format = "csv"),rio::import("https://github.com/nytimes/covid-19-data/raw/master/us-counties-2021.csv",
-                                                              format = "csv")) |> 
-  full_join(rio::import("https://github.com/nytimes/covid-19-data/raw/master/us-counties-2022.csv",
-                        format = "csv")) |>
-  full_join(rio::import("https://github.com/nytimes/covid-19-data/raw/master/us-counties-2023.csv",
-                        format = "csv")) |> 
-  filter(fips == 17019)
+get_nyt_covid <- function(year) {
+  nyt_url <-  paste0(paste0("https://github.com/nytimes/covid-19-data/raw/master/us-counties-",year,".csv"))
+  nyt_csv <- rio::import(nyt_url, format = "csv")|> 
+    filter(fips == 17019)
+}
+
+nyt_data <- map(2020:year(now(tzone = "America/Chicago")),
+                get_nyt_covid) 
+nyt_data <- do.call("rbind",nyt_data)
+
 nyt_champaign <- nyt_data |> 
   mutate(Date = ymd(date)) |> 
   mutate(new_casess = cases - lag(cases, 1)) |> 
@@ -992,14 +994,16 @@ sep = ""
 # case acceleration ----
 ### get data ----
 #### Champaign ----
-nyt_data <- full_join(rio::import("https://github.com/nytimes/covid-19-data/raw/master/us-counties-2020.csv",
-                                  format = "csv"),rio::import("https://github.com/nytimes/covid-19-data/raw/master/us-counties-2021.csv",
-                                                              format = "csv")) |> 
-  full_join(rio::import("https://github.com/nytimes/covid-19-data/raw/master/us-counties-2022.csv",
-                        format = "csv")) |>
-  full_join(rio::import("https://github.com/nytimes/covid-19-data/raw/master/us-counties-2023.csv",
-                        format = "csv")) |> 
-  filter(fips == 17019)
+get_nyt_covid <- function(year) {
+  nyt_url <-  paste0(paste0("https://github.com/nytimes/covid-19-data/raw/master/us-counties-",year,".csv"))
+  nyt_csv <- rio::import(nyt_url, format = "csv")|> 
+    filter(fips == 17019)
+}
+
+nyt_data <- map(2020:year(now(tzone = "America/Chicago")),
+                get_nyt_covid) 
+nyt_data <- do.call("rbind",nyt_data)
+
 nyt_champaign <- nyt_data |> 
   mutate(Date = ymd(date)) |> 
   mutate(new_casess = cases - lag(cases, 1)) |> 
