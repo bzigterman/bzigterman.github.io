8b9655b624e1ed1428bb15271377c813b7e61e3c
Ben Zigterman <ben@bzigterman.com>
2023-04-04 15:21:37 -0500

    add volume

--- usbr.R
+++ usbr.R
@@ -4,55 +4,50 @@ library(jsonlite)
 library(janitor)
 
 # get data ----
-
-## lake mead ----
-get_mead_records <- function(page) {
-  url <- paste0("https://data.usbr.gov/rise/api/result?itemId=6123&itemsPerPage=10000&page=",page)
-  powell <- GET(url,
-                accept("application/vnd.api+json"))
-  powell <- content(powell, as = "text")
-  powell <-  fromJSON(powell, flatten = TRUE)
-  powell <- powell$data %>%
-    clean_names()
-  powell_data <- powell %>%
-    select(attributes_date_time, attributes_result) 
+get_usbr_records <- function(itemID,max_pages) {
+  data_list <- list()
+  
+  for (page_num in 1:max_pages) {
+      url <- paste0("https://data.usbr.gov/rise/api/result?itemId=",itemID,"&itemsPerPage=10000&page=",page_num)
+      response <- GET(url,
+                  accept("application/vnd.api+json"))
+      data <- fromJSON(content(response, as = "text"),flatten = TRUE)$data
+      
+      if (is.null(data)) {
+        break
+      }
+      
+      data_list[[page_num]] <- data
+  }
+  combined_data <- do.call(rbind, data_list)|> 
+    clean_names() |> 
+    select(attributes_date_time, attributes_result) |> 
+    mutate(date = as_date( ymd_hms(attributes_date_time))) |> 
+    select(!attributes_date_time)
+  
+  return(combined_data)
+  
 }
-page_1 <- get_mead_records(1)
-page_2 <- get_mead_records(2)
-page_3 <- get_mead_records(3)
-page_4 <- get_mead_records(4)
-mead_records <- full_join(page_1,
-                          page_2) %>%
-  full_join(page_3) %>%
-  full_join(page_4) %>%
-  mutate(date = as_date( ymd_hms(attributes_date_time))) %>%
-  mutate(value = attributes_result) %>%
-  select(date, value) %>%
-  filter(date >= ymd( "1935-06-25"))
+
+mead_elevation <- get_usbr_records(6123,10) |> 
+  mutate(elevation = attributes_result) |> 
+  select(!attributes_result)
+mead_volume <- get_usbr_records(6124,10)|> 
+  mutate(volume = attributes_result) |> 
+  select(!attributes_result)
+mead_records <- full_join(mead_elevation,mead_volume) |> 
+  arrange(date)
 
 write_csv(mead_records, file = "data/mead.csv")
 
-## lake powell ----
-get_powell_records <- function(page) {
-  url <- paste0("https://data.usbr.gov/rise/api/result?itemId=508&itemsPerPage=10000&page=",page)
-  powell <- GET(url,
-                accept("application/vnd.api+json"))
-  powell <- content(powell, as = "text")
-  powell <-  fromJSON(powell, flatten = TRUE)
-  powell <- powell$data %>%
-    clean_names()
-  powell_data <- powell %>%
-    select(attributes_date_time, attributes_result) 
-}
-powell_1 <- get_powell_records(1)
-powell_2 <- get_powell_records(2)
-powell_3 <- get_powell_records(3)
-powell_records <- full_join(powell_1,
-                            powell_2) %>%
-  full_join(powell_3) %>%
-  mutate(date = as_date( ymd_hms(attributes_date_time))) %>%
-  mutate(value = attributes_result) %>%
-  select(date, value)
+powell_elevation <- get_usbr_records(508,10)|> 
+  mutate(elevation = attributes_result) |> 
+  select(!attributes_result)
+powell_volume <- get_usbr_records(509,10)|> 
+  mutate(volume = attributes_result) |> 
+  select(!attributes_result)
+powell_records <- full_join(powell_elevation,powell_volume) |> 
+  arrange(date)
 
 write_csv(powell_records, file = "data/powell.csv")
 
--- water.R
+++ water.R
@@ -15,12 +15,12 @@ library(jsonlite)
 ## lake mead ----
 mead_records <- read_csv(file = "data/mead.csv")
 
-latest_mead <- tail(mead_records,1)$value
-latest_mead_text <- format(round(tail(mead_records,1)$value), big.mark = ",")
+latest_mead <- tail(mead_records,1)$elevation
+latest_mead_text <- format(round(tail(mead_records,1)$elevation), big.mark = ",")
 
-year_ago_mead <- head(tail(mead_records, 365),1)$value
+year_ago_mead <- head(tail(mead_records, 365),1)$elevation
 year_ago_mead_text <- 
-  format(round(head(tail(mead_records, 365),1)$value), big.mark = ",")
+  format(round(head(tail(mead_records, 365),1)$elevation), big.mark = ",")
 year_change_mead <- round((100*(latest_mead-year_ago_mead)/year_ago_mead),1)
 year_change_mead_text <- 
   if (year_change_mead > 0) { 
@@ -30,19 +30,19 @@ year_change_mead_text <-
   } else { 
     paste(", or ",year_change_mead,"%,", sep = "")
   }
-year_change_mead_value <- value <- latest_mead - year_ago_mead
-year_change_mead_value_text <- 
-  if (round(year_change_mead_value) > 0) { 
-    paste("up ",format(round(year_change_mead_value),big.mark = ","),"", sep = "")
-  } else if (round(year_change_mead_value) == 0) {
+year_change_mead_elevation <- elevation <- latest_mead - year_ago_mead
+year_change_mead_elevation_text <- 
+  if (round(year_change_mead_elevation) > 0) { 
+    paste("up ",format(round(year_change_mead_elevation),big.mark = ","),"", sep = "")
+  } else if (round(year_change_mead_elevation) == 0) {
     paste("unchanged", sep = "")
   } else { 
-    paste("down ",format(round(-1*year_change_mead_value),big.mark = ","),"", sep = "")
+    paste("down ",format(round(-1*year_change_mead_elevation),big.mark = ","),"", sep = "")
   }
 
-ten_year_ago_mead <- head(tail(mead_records, 3652),1)$value
+ten_year_ago_mead <- head(tail(mead_records, 3652),1)$elevation
 ten_year_ago_mead_text <- 
-  format(round(head(tail(mead_records, 3652),1)$value), big.mark = ",")
+  format(round(head(tail(mead_records, 3652),1)$elevation), big.mark = ",")
 ten_year_change_mead <- round((100*(latest_mead-ten_year_ago_mead)/ten_year_ago_mead),1)
 ten_year_change_mead_text <- 
   if (ten_year_change_mead > 0) { 
@@ -52,29 +52,29 @@ ten_year_change_mead_text <-
   } else { 
     paste(", or ",ten_year_change_mead,"%,", sep = "")
   }
-ten_year_change_mead_value <- value <- latest_mead - ten_year_ago_mead
-ten_year_change_mead_value_text <- 
-  if (round(ten_year_change_mead_value) > 0) { 
-    paste("up ",format(round(ten_year_change_mead_value), big.mark = ","),"", sep = "")
-  } else if (round(ten_year_change_mead_value) == 0) {
+ten_year_change_mead_elevation <- elevation <- latest_mead - ten_year_ago_mead
+ten_year_change_mead_elevation_text <- 
+  if (round(ten_year_change_mead_elevation) > 0) { 
+    paste("up ",format(round(ten_year_change_mead_elevation), big.mark = ","),"", sep = "")
+  } else if (round(ten_year_change_mead_elevation) == 0) {
     paste("unchanged", sep = "")
   } else { 
-    paste("down ",format(round(-1*ten_year_change_mead_value), big.mark = ","),"", sep = "")
+    paste("down ",format(round(-1*ten_year_change_mead_elevation), big.mark = ","),"", sep = "")
   }
 
-full_pool_mead <- 1229 - tail(mead_records,1)$value
+full_pool_mead <- 1229 - tail(mead_records,1)$elevation
 full_pool_mead_text <- format(round(full_pool_mead), big.mark = ",")
-dead_pool_mead <- tail(mead_records,1)$value - 895
+dead_pool_mead <- tail(mead_records,1)$elevation - 895
 dead_pool_mead_text <- format(round(dead_pool_mead), big.mark = ",")
 
 ## lake powell -----
 powell_records <- read_csv(file = "data/powell.csv")
 
-latest_powell <- tail(powell_records,1)$value
-latest_powell_text <- format(round(tail(powell_records,1)$value), big.mark = ",")
-year_ago_powell <- head(tail(powell_records, 365),1)$value
+latest_powell <- tail(powell_records,1)$elevation
+latest_powell_text <- format(round(tail(powell_records,1)$elevation), big.mark = ",")
+year_ago_powell <- head(tail(powell_records, 365),1)$elevation
 year_ago_powell_text <- 
-  format(round(head(tail(powell_records, 365),1)$value), big.mark = ",")
+  format(round(head(tail(powell_records, 365),1)$elevation), big.mark = ",")
 year_change_powell <- round((100*(latest_powell-year_ago_powell)/year_ago_powell),1)
 year_change_powell_text <- 
   if (year_change_powell > 0) { 
@@ -84,18 +84,18 @@ year_change_powell_text <-
   } else { 
     paste(", or ",year_change_powell,"%,", sep = "")
   }
-year_change_powell_value <- value <- latest_powell - year_ago_powell
-year_change_powell_value_text <- 
-  if (round(year_change_powell_value) > 0) { 
-    paste("up ",format(round(year_change_powell_value),big.mark = ","),"", sep = "")
-  } else if (round(year_change_powell_value) == 0) {
+year_change_powell_elevation <- elevation <- latest_powell - year_ago_powell
+year_change_powell_elevation_text <- 
+  if (round(year_change_powell_elevation) > 0) { 
+    paste("up ",format(round(year_change_powell_elevation),big.mark = ","),"", sep = "")
+  } else if (round(year_change_powell_elevation) == 0) {
     paste("unchanged", sep = "")
   } else { 
-    paste("down ",format(round(-1*year_change_powell_value),big.mark = ","),"", sep = "")
+    paste("down ",format(round(-1*year_change_powell_elevation),big.mark = ","),"", sep = "")
   }
-ten_year_ago_powell <- head(tail(powell_records, 3652),1)$value
+ten_year_ago_powell <- head(tail(powell_records, 3652),1)$elevation
 ten_year_ago_powell_text <- 
-  format(round(head(tail(powell_records, 3652),1)$value), big.mark = ",")
+  format(round(head(tail(powell_records, 3652),1)$elevation), big.mark = ",")
 ten_year_change_powell <- round((100*(latest_powell-ten_year_ago_powell)/ten_year_ago_powell),1)
 ten_year_change_powell_text <- 
   if (ten_year_change_powell > 0) { 
@@ -105,47 +105,62 @@ ten_year_change_powell_text <-
   } else { 
     paste(", or ",ten_year_change_powell,"%,", sep = "")
   }
-ten_year_change_powell_value <- value <- latest_powell - ten_year_ago_powell
-ten_year_change_powell_value_text <- 
-  if (round(ten_year_change_powell_value) > 0) { 
-    paste("up ",format(round(ten_year_change_powell_value),big.mark = ","),"", sep = "")
-  } else if (round(ten_year_change_powell_value) == 0) {
+ten_year_change_powell_elevation <- elevation <- latest_powell - ten_year_ago_powell
+ten_year_change_powell_elevation_text <- 
+  if (round(ten_year_change_powell_elevation) > 0) { 
+    paste("up ",format(round(ten_year_change_powell_elevation),big.mark = ","),"", sep = "")
+  } else if (round(ten_year_change_powell_elevation) == 0) {
     paste("unchanged", sep = "")
   } else { 
-    paste("down ",format(round(-1*ten_year_change_powell_value),big.mark = ","),"", sep = "")
+    paste("down ",format(round(-1*ten_year_change_powell_elevation),big.mark = ","),"", sep = "")
   }
 
-full_pool_powell <- 3708.34 - tail(powell_records,1)$value
+full_pool_powell <- 3708.34 - tail(powell_records,1)$elevation
 full_pool_powell_text <- format(round(full_pool_powell), big.mark = ",")
-dead_pool_powell <- tail(powell_records,1)$value - 3370
+dead_pool_powell <- tail(powell_records,1)$elevation - 3370
 dead_pool_powell_text <- format(round(dead_pool_powell), big.mark = ",")
 
 # charts ----
 ## mead ----
-fig <- hchart(mead_records, "line", hcaes(x = date,
-                                          y = value),
-              color = "#199fa8",
-              animation = FALSE,
-              name = "Level") %>%
-  hc_yAxis(title = "",
-           plotLines = list(
-             list(
-               label = list(text = "Min",
-                            align = "right",
-                            x = -5),
-               color = "#808080",
-               width = 1.5,
-               value = 895,
-               zIndex = 1),
-             list(
-               label = list(text = "Max",
-                            align = "right",
-                            x = -5),
-               color = "#808080",
-               width = 1.5,
-               value = 1229,
-               zIndex = 1))) %>%
-  hc_xAxis(title = "") %>%
+fig <- highchart() |> 
+  hc_add_series(mead_records, "line", hcaes(x = date,
+                                            y = elevation),
+                color = "#199fa8",
+                yAxis = 0,
+                animation = FALSE,
+                name = "Elevation") %>%
+  hc_add_series(mead_records, "line", hcaes(x = date,
+                                            y = volume),
+                color = "red",
+                animation = FALSE,
+                yAxis = 1,
+                name = "Volume") |> 
+  hc_yAxis_multiples(create_axis(naxis = 2, 
+                                 heights = c(1,1),
+                                 title = list(text = NULL),
+                                 plotLines = list(
+                                   list(
+                                     list(
+                                       label = list(text = "Min",
+                                                    align = "right",
+                                                    x = -5),
+                                       color = "#808080",
+                                       width = 1.5,
+                                       value = 895,
+                                       zIndex = 1),
+                                     list(
+                                       label = list(text = "Max",
+                                                    align = "right",
+                                                    x = -5),
+                                       color = "#808080",
+                                       width = 1.5,
+                                       value = 1229,
+                                       zIndex = 1)),
+                                   NA))) |>  
+  hc_xAxis(title = "",
+           type = "datetime") %>%
+  hc_tooltip(shared = TRUE,
+             split = TRUE) |> 
   hc_add_theme(
     hc_theme_bloom()
   ) %>%
@@ -162,30 +177,45 @@ saveWidget(widget = fig, file = "interactive/lake_mead_water_level.html",
            libdir = "interactive")
 
 ## powell ----
-fig <- hchart(powell_records, "line", hcaes(x = date,
-                                            y = value),
-              color = "#199fa8",
-              animation = FALSE,
-              name = "Level") %>%
-  hc_yAxis(title = "",
-           plotLines = list(
-             list(
-               label = list(text = "Min",
-                            align = "right",
-                            x = -5),
-               color = "#808080",
-               width = 1.5,
-               value = 3370,
-               zIndex = 1),
-             list(
-               label = list(text = "Max",
-                            align = "right",
-                            x = -5),
-               color = "#808080",
-               width = 1.5,
-               value = 3708.34,
-               zIndex = 1))) %>%
-  hc_xAxis(title = "") %>%
+fig <- highchart() |> 
+  hc_add_series(powell_records, "line", hcaes(x = date,
+                                              y = elevation),
+                color = "#199fa8",
+                yAxis = 0,
+                animation = FALSE,
+                name = "Elevation") %>%
+  hc_add_series(powell_records, "line", hcaes(x = date,
+                                              y = volume),
+                color = "red",
+                animation = FALSE,
+                yAxis = 1,
+                name = "Volume") |> 
+  hc_yAxis_multiples(create_axis(naxis = 2, 
+                                 heights = c(1,1),
+                                 title = list(text = NULL),
+                                 plotLines = list(
+                                   list(
+                                     list(
+                                       label = list(text = "Min",
+                                                    align = "right",
+                                                    x = -5),
+                                       color = "#808080",
+                                       width = 1.5,
+                                       value = 3370,
+                                       zIndex = 1),
+                                     list(
+                                       label = list(text = "Max",
+                                                    align = "right",
+                                                    x = -5),
+                                       color = "#808080",
+                                       width = 1.5,
+                                       value = 3708.34,
+                                       zIndex = 1)),
+                                   NA))) |>  
+  hc_xAxis(title = "",
+           type = "datetime") %>%
+  hc_tooltip(shared = TRUE,
+             split = TRUE) |> 
   hc_add_theme(
     hc_theme_bloom()
   ) %>%
@@ -219,8 +249,8 @@ Elevation:
 - ",latest_mead_text," feet
 - ",dead_pool_mead_text," feet above the minimum level
 - ",full_pool_mead_text," feet below the maximum level
-- ",year_change_mead_value_text," feet from a year ago
-- ",ten_year_change_mead_value_text," feet from ten years ago
+- ",year_change_mead_elevation_text," feet from a year ago
+- ",ten_year_change_mead_elevation_text," feet from ten years ago
 
 ## [Lake Powell](https://en.wikipedia.org/wiki/Lake_Powell)
 
@@ -231,8 +261,8 @@ Elevation:
 - ",latest_powell_text," feet
 - ",dead_pool_powell_text," feet above the minimum level
 - ",full_pool_powell_text," feet below the maximum level
-- ",year_change_powell_value_text," feet from a year ago
-- ",ten_year_change_powell_value_text," feet from ten years ago
+- ",year_change_powell_elevation_text," feet from a year ago
+- ",ten_year_change_powell_elevation_text," feet from ten years ago
 
 <p class=\"updated_time\">Source: <a href=\"https://www.usbr.gov/lc/region/g4000/riverops/hourly7.html#t0\">U.S. Bureau of Reclamation</a>.</p> 
 
