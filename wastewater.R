3ffeb3a61c77bc9a3c9663d57af7ab3f686dc455
Ben Zigterman <ben@bzigterman.com>
2023-04-14 15:13:22 -0500

    fix reference

--- wastewater.R
+++ wastewater.R
@@ -95,7 +95,7 @@ imageurl: https://raw.githubusercontent.com/bzigterman/CUcovid/main/gh_action/Ch
 
 ## Champaign County Wastewater Surveillance
 
-<iframe src=\"/interactive/champaign_wastewater.html\" width=\"100%\" height=\"400\"> 
+<iframe src=\"/interactive/wastewater.html\" width=\"100%\" height=\"400\"> 
 </iframe>
 
 More information available from the [CDC](https://covid.cdc.gov/covid-data-tracker/#wastewater-surveillance) and the [Illinois Wastewater Surveillance System](https://iwss.uillinois.edu/wastewater-treatment-plant/159/).
