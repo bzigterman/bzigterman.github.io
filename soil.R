4afa7b393306542376e6bd46a99856d7834018e8
Ben Zigterman <ben@bzigterman.com>
2023-05-24 09:55:16 -0500

    Update soil.R

--- soil.R
+++ soil.R
@@ -364,7 +364,7 @@ webappicon: /weather.png
 
 ## Soil Moisture
 
-<iframe src=\"/interactive/soil_temps.html\" width=\"100%\" height=\"400\"> 
+<iframe src=\"/interactive/soil_moisture.html\" width=\"100%\" height=\"400\"> 
 </iframe>
 
 ## Latest Freeze Dates
