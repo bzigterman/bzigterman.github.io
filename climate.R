d5d24c0a19d5678f24998db5db245a1077cfadbf
Ben Zigterman <ben@bzigterman.com>
2023-09-07 14:42:37 -0500

    adjust title

--- climate.R
+++ climate.R
@@ -93,7 +93,7 @@ permalink: /projects/climate
 imageurl: https://raw.githubusercontent.com/bzigterman/CUcovid/main/gh_action/Champaign_facet.png
 ---
 
-## Global Mean Estimates based on Land and Ocean Data
+## Global Average Temperature
 
 <iframe src=\"/interactive/climate_temp_anomaly.html\" width=\"100%\" height=\"400\"> 
 </iframe>
