55bcf9bb096521476c773d410da03d93df9cc564
Ben Zigterman <ben@bzigterman.com>
2023-11-14 20:19:49 -0600

    add chart embed

--- climate.R
+++ climate.R
@@ -122,6 +122,9 @@ imageurl: https://raw.githubusercontent.com/bzigterman/CUcovid/main/gh_action/Ch
 
 <iframe src=\"https://cdn.climatechangetracker.org/embedding/yearly-average-temperature-anomaly?tempunit=c&shadow=false&br=false\" scrolling=\"no\" frameBorder=\"0\" style=\"width:100%; height:400px\"></iframe>
 
+<iframe src=\"https://cdn.climatechangetracker.org/embedding/human-induced-and-observed-global-warming?tempunit=c&shadow=false&br=false\" scrolling=\"no\" frameBorder=\"0\" style=\"width:100%; height:400px\"></iframe>
+
+
 ",
 sep = ""
 )
