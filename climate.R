13239c712d83d2f4d0fb734577341508243b8dbd
Ben Zigterman <ben@bzigterman.com>
2023-11-14 20:26:53 -0600

    adjust formatting

--- climate.R
+++ climate.R
@@ -120,10 +120,9 @@ imageurl: https://raw.githubusercontent.com/bzigterman/CUcovid/main/gh_action/Ch
 
 ## Global Average Temperature
 
-<iframe src=\"https://cdn.climatechangetracker.org/embedding/yearly-average-temperature-anomaly?tempunit=c&shadow=false&br=false\" scrolling=\"no\" frameBorder=\"0\" style=\"width:100%; height:400px\"></iframe>
+<iframe src=\"https://cdn.climatechangetracker.org/embedding/yearly-average-temperature-anomaly?tempunit=c\" scrolling=\"no\" frameBorder=\"0\" style=\"width:100%; height:400px\"></iframe>
 
-<iframe src=\"https://cdn.climatechangetracker.org/embedding/human-induced-and-observed-global-warming?tempunit=c&shadow=false&br=false\" scrolling=\"no\" frameBorder=\"0\" style=\"width:100%; height:400px\"></iframe>
-
+<iframe src=\"https://cdn.climatechangetracker.org/embedding/human-induced-and-observed-global-warming?tempunit=c\" scrolling=\"no\" frameBorder=\"0\" style=\"width:100%; height:400px\"></iframe>
 
 ",
 sep = ""
