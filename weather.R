4a07fcf7bbc88300db690c3e433f1f8965387b02
Ben Zigterman <ben@bzigterman.com>
2024-01-11 12:35:46 -0600

    add model lines

--- weather.R
+++ weather.R
@@ -137,7 +137,9 @@ om_temp_hourly <- as_tibble( om$hourly) |>
   select(!temperature_2m_gem_seamless) |> 
   mutate(MeteoFrance = temperature_2m_meteofrance_seamless) |> 
   select(!temperature_2m_meteofrance_seamless) |> 
-  pivot_longer(!c(time,datetime)) |> 
+  pivot_longer(!c(time,datetime)) 
+
+om_temp_hourly_min_max <- om_temp_hourly |> 
   group_by(time) |> 
   mutate(max = max(value, na.rm = TRUE)) |> 
   mutate(min = min(value, na.rm = TRUE)) |> 
@@ -169,7 +171,7 @@ global$timezoneOffset <- offset
 options(highcharter.global = global)
 
 fig <- highchart() |> 
-  hc_add_series(data = om_temp_hourly, ### temperature ----
+  hc_add_series(data = om_temp_hourly_min_max, ### temperature ----
                 type = "arearange",
                 name = "Temperature",
                 states = list(
@@ -202,13 +204,56 @@ fig <- highchart() |>
                   c(value = 95,  color = "#B6493B"),
                   c(value = 200, color = "#A44139")),
                 color = "black",
-                lineWidth = 1,
+                opacity = .1,
+                lineWidth = 0,
                 connectNulls = TRUE,
                 tooltip = list(valueSuffix = "°",
                                valueDecimals = 0),
                 hcaes(x = time*1000,
                       low = min,
                       high = max),
+                yAxis = 0) |> 
+  hc_add_series(data = om_temp_hourly,
+                type = "line",
+                #name = "Temperature",
+                enableMouseTracking = FALSE,
+                states = list(
+                  inactive = list(
+                    enabled = FALSE
+                  )
+                ),
+                label = list(
+                  enabled = FALSE),
+                zones = list(
+                  c(value = 0,   color = "#F8D4FC"),
+                  c(value = 5,   color = "#E5A4EB"),
+                  c(value = 10,  color = "#D392DD"),
+                  c(value = 15,  color = "#C07ECC"),
+                  c(value = 20,  color = "#9D63C2"),
+                  c(value = 25,  color = "#794DB4"),
+                  c(value = 30,  color = "#5B4FA6"),
+                  c(value = 32,  color = "#527DC7"),
+                  c(value = 40,  color = "#65C1DE"),
+                  c(value = 45,  color = "#6EDAE0"),
+                  c(value = 50,  color = "#6EDBA2"),
+                  c(value = 55,  color = "#69C954"),
+                  c(value = 60,  color = "#93D452"),
+                  c(value = 65,  color = "#E3E65B"),
+                  c(value = 70,  color = "#FFFF61"),
+                  c(value = 75,  color = "#F8D456"),
+                  c(value = 80,  color = "#ED9749"),
+                  c(value = 85,  color = "#DC6641"),
+                  c(value = 90,  color = "#CA593E"),
+                  c(value = 95,  color = "#B6493B"),
+                  c(value = 200, color = "#A44139")),
+                color = "black",
+                lineWidth = 2,
+                connectNulls = TRUE,
+                tooltip = list(valueSuffix = "°",
+                               valueDecimals = 0),
+                hcaes(x = time*1000,
+                      y = value,
+                      group = name),
                 yAxis = 0) |> 
   # hc_add_series(data = om_hourly, ### feels like 
   #               type = "line",
