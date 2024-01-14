dd9b0b43fb677bb8d691731b93f46e134351c2d0
Ben Zigterman <ben@bzigterman.com>
2024-01-13 20:35:04 -0600

    fix formatting

--- weather.R
+++ weather.R
@@ -1817,8 +1817,7 @@ if (champaign_any_snow > 0) {
     paste("## Two-Day Snow Potential
           
           <iframe src=\"/interactive/champaign_snow_chance.html\" width=\"100%\" height=\"250\"> 
-</iframe>
-")
+          </iframe>")
 } else {
   champaign_snow_forecast <- ""
 }
