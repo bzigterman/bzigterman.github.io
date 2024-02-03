5a970a23d2b0a61dcd6d92d41dabb44cf229051a
Ben Zigterman <ben@bzigterman.com>
2024-02-02 19:59:53 -0600

    fix adding new frame

--- timelapse.R
+++ timelapse.R
@@ -5,7 +5,10 @@ url <- "https://cctv.travelmidwest.com/snapshots/IL-IDOTD4_5_Champaign_SEB_I-74_
 
 animation <- image_read("timelapse/animation.gif")
 frames <- length(animation)
+new_frame <- frames+1
 latest <- image_read(url)
 updated_animation <- animation[c(1:frames,frames)]
+updated_animation[new_frame] <- latest
+
 image_write(image = updated_animation,
             path = "timelapse/animation.gif")
