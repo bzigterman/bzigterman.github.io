3d03f5b2fa75dff606f978b26dd7cc80f01088ba
Ben Zigterman <ben@bzigterman.com>
2024-05-01 11:28:10 -0500

    remove background for 0%

--- basketball.R
+++ basketball.R
@@ -540,7 +540,7 @@ if (length(standings_the_same) > 0) {
     ) |> 
     data_color(
       columns = c(post,finals),
-      domain = c(0,100),
+      domain = c(0.1,100),
       na_color = "#FFFFFF",
       palette = "Reds"
     ) |> 
