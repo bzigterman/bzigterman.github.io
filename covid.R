d40489f61b79b38a9a31a3dacbd2e06deb65617b
Ben Zigterman <ben@bzigterman.com>
2025-02-13 17:18:17 -0600

    adjust text

--- covid.R
+++ covid.R
@@ -471,7 +471,7 @@ webappicon: /covid.png
 
 Charts for Champaign County are posted weekly on Mastodon <a rel=\"me\" href=\"https://mastodon.social/@ChampaignCovid\">@ChampaignCovid@mastodon.social</a>.
 
-More information about wastewater surveillance available from the [Illinois Wastewater Surveillance System](https://iwss.uillinois.edu/wastewater-treatment-plant/159/).
+More information about wastewater surveillance is available from the [Illinois Wastewater Surveillance System](https://iwss.uillinois.edu/wastewater-treatment-plant/159/).
 
 ## United States
 
