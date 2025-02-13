803a7544dc3d1330eb456b2c3fbc93e62365b59b
Ben Zigterman <ben@bzigterman.com>
2025-02-13 15:45:06 -0600

    add wastewaterbot

---
+++ wastewaterbot.R
@@ -0,0 +1,116 @@
+library(tidyverse)
+library(rtoot)
+library(scales)
+library(httr)
+
+# mastodon api setup ----
+token <- Sys.getenv("RTOOT_DEFAULT_TOKEN")
+verify_envvar(verbose = TRUE)
+
+iwss_download_url <- "https://iwss.uillinois.edu/wastewater-treatment-plant/download/159/"
+iwss_download <- content(GET(iwss_download_url))
+
+# data ----
+
+iwss <- iwss_download %>%
+  mutate(Date = ymd(sample_collect_date)) %>%
+  filter(method == 1) |> 
+  mutate(sars_cov_2_avg = zoo::rollmean(sars_cov_2,
+                                        k = 4,
+                                        fill = NA,
+                                        align = "right")) |> 
+  mutate(influenza_a_avg = zoo::rollmean(influenza_a,
+                                         k = 4,
+                                         fill = NA,
+                                         align = "right")) |> 
+  mutate(influenza_b_avg = zoo::rollmean(influenza_b,
+                                         k = 4,
+                                         fill = NA,
+                                         align = "right")) |> 
+  mutate(rsv_avg = zoo::rollmean(rsv,
+                                 k = 4,
+                                 fill = NA,
+                                 align = "right"))
+iwss_longer <- iwss |> 
+  select(sars_cov_2,influenza_a,influenza_b,rsv,Date,
+         sars_cov_2_avg,influenza_a_avg,influenza_b_avg,rsv_avg) |> 
+  pivot_longer(!Date) |> 
+  filter(Date > today(tzone = "America/Chicago")-years(1) ) |> 
+  mutate(contains_avg = str_detect(name, "avg")) |> 
+  mutate(contains_avg = if_else(contains_avg,
+                                "avg",
+                                "not_avg")) |> 
+  mutate(name = recode_factor(name, 
+                              "sars_cov_2_avg" = "SARS-CoV-2",
+                              "influenza_a_avg" = "Influenza A",
+                              "influenza_b_avg" = "Influenza B",
+                              "rsv_avg" = "RSV"))
+
+latest_data <- tail(iwss_longer, n = 1)$Date
+
+# chart ----
+
+all_colors <- c(
+  #"#f0dfcd","#e5e5ff","#f2e5f2","lightgray",
+  "#B45F06","blue","purple","black"
+)
+
+p <- ggplot() +
+  geom_line(data = iwss_longer |> filter(contains_avg == "not_avg"),
+            aes(x = as.Date(Date),
+                y = value,
+                group = name),
+            color = "lightgray",
+            show.legend = FALSE,
+            size = .4, alpha = .75) +
+  geom_line(data = iwss_longer |> filter(contains_avg == "avg"),
+            aes(x = as.Date(Date),
+                y = value,
+                color = name),
+            size = 1) +
+  scale_colour_manual(#guide = "none",
+    values = all_colors) +
+  labs(caption = paste("Source: IWSS")) +
+  xlab(NULL) +
+  ylab("Gene Copies Per Liter (w/ Moving Avg.)") +
+  scale_x_date(expand = c(0,0)) +
+  scale_y_continuous(#labels = label_comma(accuracy = 1),
+                     labels = label_number(scale_cut = cut_short_scale()),
+                     #position = "right",
+                     expand = expansion(mult = c(0,.05))
+  ) +
+  expand_limits(y = 0) +
+  theme(axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 8),
+        legend.title = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major.y = element_line(colour = "grey93"),
+        strip.text = element_text(size = 11),
+        strip.background = element_blank(),
+        plot.caption = element_text(colour = "grey40"))
+p
+# save to a temp file
+file <- tempfile( fileext = ".png")
+ggsave( file, plot = p, device = "png", dpi = 320, width = 4, height = 4)
+
+# make text ----
+
+champaign_county_text <- paste(
+  "More data: https://bzigterman.com/projects/wastewater",
+  sep = ""
+)
+champaign_county_text
+typeof(champaign_county_text)
+
+# post ----
+if (champaign_avg_hospitalized >= 0 && 
+    champaign_month_ago_hospitalized >= 0 && 
+    latest_data >= now(tzone = "America/Chicago") - days(x = 14)
+) {
+  post_toot(status = champaign_county_text,
+            media = file,
+            alt_text = "Line charts with recent wastewater statistics for Champaign County, Illinois")
+}
+
+
