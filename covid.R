ee18111bc0ed409e36df15f84b39b3342e943b84
Ben Zigterman <ben@bzigterman.com>
2025-02-21 11:48:11 -0600

    update formatting

--- covid.R
+++ covid.R
@@ -18,43 +18,50 @@ iwss_download <- content(GET(iwss_download_url))
 
 iwss <- iwss_download %>%
   mutate(Date = ymd(sample_collect_date)) %>%
-  filter(method == 1) |> 
-  select(Date, sars_cov_2, method) |> 
-  mutate(sars_cov_2_avg = zoo::rollmean(sars_cov_2,
-                                        k = 4,
-                                        fill = NA,
-                                        align = "right")) 
+  filter(method == 1) |>
+  select(Date, sars_cov_2, method) |>
+  mutate(
+    sars_cov_2_avg = zoo::rollmean(
+      sars_cov_2,
+      k = 4,
+      fill = NA,
+      align = "right"
+    )
+  )
 
-combined <- iwss |> 
-  arrange(Date) |> 
-  #fill(avg_hospitalized,.direction = "down") |> 
-  fill(sars_cov_2_avg,.direction = "down")
+combined <- iwss |>
+  arrange(Date) |>
+  #fill(avg_hospitalized,.direction = "down") |>
+  fill(sars_cov_2_avg, .direction = "down")
 
-fig <- hchart(combined,
-              type = "line", 
-              hcaes(x = Date,
-                    y = sars_cov_2_avg),
-              states = list(
-                inactive = list(
-                  enabled = FALSE
-                )
-              ),
-              tooltip = list(valueDecimals = 0),
-              connectNulls = TRUE,
-              name = "Gene Copies Per Liter (w/ Moving Avg.)",
-              label = list(
-                format = "Wastewater",
-                enabled = TRUE),
-              color = "#4e79a7",
-              yAxis = 0) %>%
-  hc_yAxis_multiples(create_axis(naxis = 1, 
-                                 #heights = c(1,1),
-                                 endOnTick = FALSE,
-                                 title = list(text = NULL))) %>%
+fig <- hchart(
+  combined,
+  type = "line",
+  hcaes(x = Date, y = sars_cov_2_avg),
+  states = list(
+    inactive = list(
+      enabled = FALSE
+    )
+  ),
+  tooltip = list(valueDecimals = 0),
+  connectNulls = TRUE,
+  name = "Gene Copies Per Liter (w/ Moving Avg.)",
+  label = list(
+    format = "Wastewater",
+    enabled = TRUE
+  ),
+  color = "#4e79a7",
+  yAxis = 0
+) %>%
+  hc_yAxis_multiples(create_axis(
+    naxis = 1,
+    #heights = c(1,1),
+    endOnTick = FALSE,
+    title = list(text = NULL)
+  )) %>%
   hc_add_series(
     data = combined,
-    hcaes(x = Date,
-          y = sars_cov_2),
+    hcaes(x = Date, y = sars_cov_2),
     name = "Gene Copies Per Liter",
     color = "#c9d6e4",
     borderWidth = 0,
@@ -69,29 +76,37 @@ fig <- hchart(combined,
     enableMouseTracking = FALSE,
     zIndex = -3,
     type = "column",
-    yAxis = 0)  %>%
+    yAxis = 0
+  ) %>%
   hc_credits(
     enabled = TRUE,
     text = "Source: CDC and IWSS",
-    href = "https://bzigterman.com/interactive/champaign_covid.html") %>%
+    href = "https://bzigterman.com/interactive/champaign_covid.html"
+  ) %>%
   hc_xAxis(title = list(text = NULL)) %>%
   hc_tooltip(shared = TRUE) %>%
   hc_add_theme(
     hc_theme_bloom()
   ) %>%
-  hc_rangeSelector(enabled = TRUE,
-                   buttons = list(
-                     list(type = 'month', count = 3, text = '3m'),
-                     list(type = 'month', count = 6, text = '6m'),
-                     list(type = 'year', count = 1, text = '1y'),
-                     list(type = 'year', count = 2, text = '2y'),
-                     list(type = 'all', text = 'All')),
-                   selected = 2)
+  hc_rangeSelector(
+    enabled = TRUE,
+    buttons = list(
+      list(type = 'month', count = 3, text = '3m'),
+      list(type = 'month', count = 6, text = '6m'),
+      list(type = 'year', count = 1, text = '1y'),
+      list(type = 'year', count = 2, text = '2y'),
+      list(type = 'all', text = 'All')
+    ),
+    selected = 2
+  )
 
 fig
-saveWidget(widget = fig, file = "interactive/champaign_covid.html",
-           selfcontained = FALSE,
-           libdir = "interactive")
+saveWidget(
+  widget = fig,
+  file = "interactive/champaign_covid.html",
+  selfcontained = FALSE,
+  libdir = "interactive"
+)
 
 ## set variables ----
 sarscov_latest <- tail(iwss, n = 1)$sars_cov_2
@@ -99,74 +114,84 @@ sarscov_latest <- tail(iwss, n = 1)$sars_cov_2
 ## usa ----
 cdc_url <- "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=us_trend_by_USA_v2"
 cdc_json <- rio::import(cdc_url, format = "json")
-cdc_usa <- cdc_json$us_trend_by_Geography_v2 |> 
-  mutate(Date = as_date(week_ending_date)) |> 
-  select(Date,
-         COVID_deaths_weekly,percent_pos)
+cdc_usa <- cdc_json$us_trend_by_Geography_v2 |>
+  mutate(Date = as_date(week_ending_date)) |>
+  select(Date, COVID_deaths_weekly, percent_pos)
 
 cdc_latest_deaths <- tail(cdc_usa, n = 1)$COVID_deaths_weekly
 
-fig <- hchart(cdc_usa,
-              type = "line", 
-              hcaes(x = Date,
-                    y = COVID_deaths_weekly),
-              states = list(
-                inactive = list(
-                  enabled = FALSE
-                )
-              ),
-              tooltip = list(valueDecimals = 0),
-              connectNulls = TRUE,
-              name = "Avg. Deaths",
-              label = list(
-                enabled = TRUE),
-              color = "black",
-              yAxis = 0) %>%
-  hc_yAxis_multiples(create_axis(naxis = 2, 
-                                 heights = c(1,1),
-                                 endOnTick = FALSE,
-                                 title = list(text = NULL))) %>%
+fig <- hchart(
+  cdc_usa,
+  type = "line",
+  hcaes(x = Date, y = COVID_deaths_weekly),
+  states = list(
+    inactive = list(
+      enabled = FALSE
+    )
+  ),
+  tooltip = list(valueDecimals = 0),
+  connectNulls = TRUE,
+  name = "Avg. Deaths",
+  label = list(
+    enabled = TRUE
+  ),
+  color = "black",
+  yAxis = 0
+) %>%
+  hc_yAxis_multiples(create_axis(
+    naxis = 2,
+    heights = c(1, 1),
+    endOnTick = FALSE,
+    title = list(text = NULL)
+  )) %>%
   hc_add_series(
     data = cdc_usa,
     connectNulls = TRUE,
-    hcaes(x = Date,
-          y = percent_pos),
+    hcaes(x = Date, y = percent_pos),
     label = list(
       #format = "Wastewater",
-      enabled = TRUE),
+      enabled = TRUE
+    ),
     states = list(
       inactive = list(
         enabled = FALSE
       )
     ),
-    tooltip = list(valueDecimals = 0,
-                   valueSuffix = "{value}%"),
+    tooltip = list(valueDecimals = 0, valueSuffix = "{value}%"),
     name = "Test Positivity",
     color = "#35978f",
     type = "line",
-    yAxis = 1) %>%
+    yAxis = 1
+  ) %>%
   hc_credits(
     enabled = TRUE,
     text = "Source: CDC",
-    href = "https://bzigterman.com/interactive/usa_covid.html") %>%
+    href = "https://bzigterman.com/interactive/usa_covid.html"
+  ) %>%
   hc_xAxis(title = list(text = NULL)) %>%
   hc_tooltip(shared = TRUE) %>%
   hc_add_theme(
     hc_theme_bloom()
   ) %>%
-  hc_rangeSelector(enabled = TRUE,
-                   buttons = list(
-                     list(type = 'month', count = 3, text = '3m'),
-                     list(type = 'month', count = 6, text = '6m'),
-                     list(type = 'year', count = 1, text = '1y'),
-                     list(type = 'year', count = 2, text = '2y'),
-                     list(type = 'all', text = 'All')),
-                   selected = 2)
+  hc_rangeSelector(
+    enabled = TRUE,
+    buttons = list(
+      list(type = 'month', count = 3, text = '3m'),
+      list(type = 'month', count = 6, text = '6m'),
+      list(type = 'year', count = 1, text = '1y'),
+      list(type = 'year', count = 2, text = '2y'),
+      list(type = 'all', text = 'All')
+    ),
+    selected = 2
+  )
 
 fig
-saveWidget(widget = fig, file = "interactive/usa_covid.html",
-           selfcontained = FALSE,
-           libdir = "interactive")
+saveWidget(
+  widget = fig,
+  file = "interactive/usa_covid.html",
+  selfcontained = FALSE,
+  libdir = "interactive"
+)
 
 ## world ----
 #### cases & deaths ----
@@ -176,48 +201,57 @@ owid_url <- "https://catalog.ourworldindata.org/garden/covid/latest/cases_deaths
 owid_data <- rio::import(owid_url, format = "csv")
 
 owid_new_cases_deaths_world <- owid_data |>
-  filter(country == "World") |> 
-  select(date,weekly_cases,weekly_deaths,
-         weekly_pct_growth_cases,weekly_pct_growth_deaths) |> 
-  mutate(avg_new_cases = weekly_cases) |> 
-  mutate(avg_new_deaths = weekly_deaths) |> 
-  mutate(pct_change_new_cases = weekly_pct_growth_cases) |> 
-  mutate(pct_change_new_deaths = weekly_pct_growth_deaths) |> 
-  mutate(Date = ymd(date)) |> 
-  mutate(date = as_date(Date)) |> 
-  mutate(location = "World") 
+  filter(country == "World") |>
+  select(
+    date,
+    weekly_cases,
+    weekly_deaths,
+    weekly_pct_growth_cases,
+    weekly_pct_growth_deaths
+  ) |>
+  mutate(avg_new_cases = weekly_cases) |>
+  mutate(avg_new_deaths = weekly_deaths) |>
+  mutate(pct_change_new_cases = weekly_pct_growth_cases) |>
+  mutate(pct_change_new_deaths = weekly_pct_growth_deaths) |>
+  mutate(Date = ymd(date)) |>
+  mutate(date = as_date(Date)) |>
+  mutate(location = "World")
 
 owid_latest_cases <- tail(owid_new_cases_deaths_world, n = 1)$weekly_cases
 owid_latest_deaths <- tail(owid_new_cases_deaths_world, n = 1)$weekly_deaths
 
-fig <- hchart(owid_new_cases_deaths_world,
-              type = "line", 
-              hcaes(x = Date,
-                    y = avg_new_cases),
-              states = list(
-                inactive = list(
-                  enabled = FALSE
-                )
-              ),
-              tooltip = list(valueDecimals = 0),
-              connectNulls = TRUE,
-              name = "Avg. Cases",
-              label = list(
-                enabled = TRUE),
-              color = "#B45F06",
-              yAxis = 0) %>%
-  hc_yAxis_multiples(create_axis(naxis = 2, 
-                                 heights = c(1,1),
-                                 endOnTick = FALSE,
-                                 title = list(text = NULL))) %>%
+fig <- hchart(
+  owid_new_cases_deaths_world,
+  type = "line",
+  hcaes(x = Date, y = avg_new_cases),
+  states = list(
+    inactive = list(
+      enabled = FALSE
+    )
+  ),
+  tooltip = list(valueDecimals = 0),
+  connectNulls = TRUE,
+  name = "Avg. Cases",
+  label = list(
+    enabled = TRUE
+  ),
+  color = "#B45F06",
+  yAxis = 0
+) %>%
+  hc_yAxis_multiples(create_axis(
+    naxis = 2,
+    heights = c(1, 1),
+    endOnTick = FALSE,
+    title = list(text = NULL)
+  )) %>%
   hc_add_series(
     data = owid_new_cases_deaths_world,
     connectNulls = TRUE,
-    hcaes(x = Date,
-          y = avg_new_deaths),
+    hcaes(x = Date, y = avg_new_deaths),
     label = list(
       #format = "Avg. New Deaths",
-      enabled = TRUE),
+      enabled = TRUE
+    ),
     states = list(
       inactive = list(
         enabled = FALSE
@@ -227,29 +261,37 @@ fig <- hchart(owid_new_cases_deaths_world,
     name = "Avg. Deaths",
     color = "#000000",
     type = "line",
-    yAxis = 1) %>%
+    yAxis = 1
+  ) %>%
   hc_credits(
     enabled = TRUE,
     text = "Source: WHO",
-    href = "https://bzigterman.com/interactive/world_covid.html") %>%
+    href = "https://bzigterman.com/interactive/world_covid.html"
+  ) %>%
   hc_xAxis(title = list(text = NULL)) %>%
   hc_tooltip(shared = TRUE) %>%
   hc_add_theme(
     hc_theme_bloom()
   ) %>%
-  hc_rangeSelector(enabled = TRUE,
-                   buttons = list(
-                     list(type = 'month', count = 3, text = '3m'),
-                     list(type = 'month', count = 6, text = '6m'),
-                     list(type = 'year', count = 1, text = '1y'),
-                     list(type = 'year', count = 2, text = '2y'),
-                     list(type = 'all', text = 'All')),
-                   selected = 2)
+  hc_rangeSelector(
+    enabled = TRUE,
+    buttons = list(
+      list(type = 'month', count = 3, text = '3m'),
+      list(type = 'month', count = 6, text = '6m'),
+      list(type = 'year', count = 1, text = '1y'),
+      list(type = 'year', count = 2, text = '2y'),
+      list(type = 'all', text = 'All')
+    ),
+    selected = 2
+  )
 
 fig
-saveWidget(widget = fig, file = "interactive/world_covid.html",
-           selfcontained = FALSE,
-           libdir = "interactive")
+saveWidget(
+  widget = fig,
+  file = "interactive/world_covid.html",
+  selfcontained = FALSE,
+  libdir = "interactive"
+)
 
 # make web text ----
 ## covid ----
@@ -282,13 +324,14 @@ More information about wastewater surveillance is available from the [Illinois W
 </iframe>
 
 ",
-sep = ""
+  sep = ""
 )
 
-if (sarscov_latest >= 0 && 
-    cdc_latest_deaths >= 0 && 
-    owid_latest_cases >= 0 && 
-    owid_latest_deaths >= 0 
+if (
+  sarscov_latest >= 0 &&
+    cdc_latest_deaths >= 0 &&
+    owid_latest_cases >= 0 &&
+    owid_latest_deaths >= 0
 ) {
-  write_lines(web_text,"projects/covid.md")
+  write_lines(web_text, "projects/covid.md")
 }
