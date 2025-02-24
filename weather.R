990eaabed74bd3e1a4fd1e81b0e16f2d2ab85a98
Ben Zigterman <ben@bzigterman.com>
2025-02-24 14:12:37 -0600

    fix broken link

--- weather.R
+++ weather.R
@@ -2075,7 +2075,7 @@ severe_weather_outlook_url <-
 winter_storm_url <-
   paste(
     "![](",
-    "https://origin.wpc.ncep.noaa.gov/wwd/wssi/images/WSSI_Overall_CONUS.png",
+    "https://www.wpc.ncep.noaa.gov/wwd/wssi/images/WSSI_Overall_CONUS.png",
     ")",
     sep = ""
   )
