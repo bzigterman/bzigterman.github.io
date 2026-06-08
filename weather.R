1918fb34b36f6e33bb9749296e66f49a33153bde
Ben Zigterman <ben@bzigterman.com>
2026-06-08 16:06:55 -0500

    make daily chart wider

--- weather.R
+++ weather.R
@@ -64,7 +64,7 @@ day_names <- format(dates, "%a")
 # 2. Define Chart Dimensions
 scale_min <- min(lows) - 2
 scale_max <- max(highs) + 2
-total_width <- 25
+total_width <- 45
 
 # 3. Generate the Text Bars
 lines <- c()
